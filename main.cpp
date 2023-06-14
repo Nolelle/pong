55f91cc6875f68f6eab4460ca8ed1b462b57f4ea
Nolelle <eyu1@uablberta.ca>
2023-06-13 18:31:27 -0600

    now allows for two players to move paddles at once

--- main.cpp
+++ main.cpp
@@ -1,44 +1,43 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Graphics/RectangleShape.hpp>
 #include <SFML/Window/Keyboard.hpp>
+#include <cwchar>
 
 int main() {
   sf::RenderWindow window(sf::VideoMode(800, 600), "Pong");
   window.setFramerateLimit(60);
 
   sf::RectangleShape leftPaddle(sf::Vector2f(25, 100));
-  sf::RectangleShape rightPaddle(sf::Vector2f(25, 100));
   leftPaddle.setFillColor(sf::Color::White);
-  rightPaddle.setFillColor(sf::Color::White);
   leftPaddle.setPosition(50, 250);
-  rightPaddle.setPosition(700, 250);
   float paddleLeftSy = 5.0f;
+
+  sf::RectangleShape rightPaddle(sf::Vector2f(25, 100));
+  rightPaddle.setFillColor(sf::Color::White);
+  rightPaddle.setPosition(725, 250);
   float paddleRightSy = 5.0f;
 
   while (window.isOpen()) {
     sf::Event event;
+
     while (window.pollEvent(event)) {
       if (event.type == sf::Event::Closed)
         window.close();
     }
-
-    if (event.type == sf::Event::KeyPressed) {
-      if (event.key.code == sf::Keyboard::Up) {
-        leftPaddle.move(0, -paddleLeftSy);
-      }
-      if (event.key.code == sf::Keyboard::Down) {
-        leftPaddle.move(0, paddleLeftSy);
-      }
-
-      if (event.key.code == sf::Keyboard::W) {
-        rightPaddle.move(0, -paddleRightSy);
-      }
-
-      if (event.key.code == sf::Keyboard::S) {
-        rightPaddle.move(0, paddleRightSy);
-      }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up)) {
+      leftPaddle.move(0, -paddleLeftSy);
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down)) {
+      leftPaddle.move(0, paddleLeftSy);
     }
 
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
+      rightPaddle.move(0, -paddleRightSy);
+    }
+
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {
+      rightPaddle.move(0, paddleRightSy);
+    }
     if (leftPaddle.getPosition().y < 0) {
       leftPaddle.setPosition(leftPaddle.getPosition().x, 0);
     }
@@ -64,6 +63,5 @@ int main() {
     window.draw(rightPaddle);
     window.display();
   }
-
   return 0;
 }
