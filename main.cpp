c86cf39b202dab711632521079021682ada41b17
Nolelle <eyu1@uablberta.ca>
2023-06-15 18:29:59 -0600

    finish ball to wall collision detection

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Graphics/CircleShape.hpp>
 #include <SFML/Graphics/RectangleShape.hpp>
+#include <SFML/System/Vector2.hpp>
 #include <SFML/Window/Keyboard.hpp>
+#include <iostream>
 
 int main() {
   sf::RenderWindow window(sf::VideoMode(800, 600), "Pong");
@@ -17,11 +19,10 @@ int main() {
   rightPaddle.setPosition(725, 250);
   float paddleRightSy = 5.0f;
 
-  sf::CircleShape ball(50.f);
+  sf::CircleShape ball(10);
   ball.setFillColor(sf::Color::White);
   ball.setPosition(375, 250);
-  float ballSx = 2.0f;
-  float ballSy = 2.0f;
+  sf::Vector2f ballVelocity(2.0f, 0.0f);
 
   while (window.isOpen()) {
     sf::Event event;
@@ -30,6 +31,8 @@ int main() {
       if (event.type == sf::Event::Closed)
         window.close();
     }
+
+    // Paddle Input
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up)) {
       leftPaddle.move(0, -paddleLeftSy);
     }
@@ -44,6 +47,8 @@ int main() {
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {
       rightPaddle.move(0, paddleRightSy);
     }
+
+    // Paddle Collision Detection
     if (leftPaddle.getPosition().y < 0) {
       leftPaddle.setPosition(leftPaddle.getPosition().x, 0);
     }
@@ -63,6 +68,27 @@ int main() {
       rightPaddle.setPosition(rightPaddle.getPosition().x,
                               window.getSize().y - rightPaddle.getSize().y);
     }
+
+    // ball movement
+    ball.move(ballVelocity.x, ballVelocity.y);
+
+    // ball collision detection
+    // Circleshape origin is at top left of circle (0,0)
+    if (ball.getPosition().x < 0 ||
+        ball.getPosition().x + (2 * ball.getRadius()) > window.getSize().x) {
+      std::cout << "X: " << ballVelocity.x << "\n";
+      ballVelocity.x *= -1;
+      std::cout << "X: " << ballVelocity.x << "\n";
+    }
+    if (ball.getPosition().y < 0 ||
+        ball.getPosition().y + (2 * ball.getRadius()) > window.getSize().y) {
+      std::cout << "Y: " << ballVelocity.y << "\n";
+      ballVelocity.y *= -1;
+      std::cout << "Y: " << ballVelocity.y << "\n";
+    }
+
+    // Ball to paddle collision detection
+    // To do
 
     window.clear();
     window.draw(leftPaddle);
